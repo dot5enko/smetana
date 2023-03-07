e754b3f6fa59bf8aceaaaa1d88df3dcc1cea0779
serhii <sergiydotsenko@gmail.com>
2023-03-07 22:29:00 +0100

    not found message

--- src/popup/screens/TrackNewAddress.tsx
+++ src/popup/screens/TrackNewAddress.tsx
@@ -25,13 +25,12 @@ export function TrackNewAddress(props: TrackNewAddressProps) {
     const [validAddr, setValid] = useState("");
     const [loading, setLoading] = useState(false);
     const [raw, setRaw] = useState<RawAccountInfo | undefined>(undefined);
+    const [found, setFound] = useState(false);
 
     useEffect(() => {
         if (validAddr != "") {
-            console.log(`fetch valid address ${addr}`)
             setLoading(true);
             getAddressInfo(validAddr, connection).then(resp => {
-
                 if (resp.value != undefined) {
 
                     const rawaccount: RawAccountInfo = {
@@ -42,9 +41,13 @@ export function TrackNewAddress(props: TrackNewAddressProps) {
                         owner: resp.value.owner.toBase58()
                     }
 
+                    setFound(true);
                     setRaw(rawaccount)
+                } else {
+                    setFound(false);
                 }
             }).catch(e => {
+                setFound(false);
                 console.error('unable to fetch address info : ', e.message)
             }).finally(() => {
                 setLoading(false)
@@ -66,28 +69,32 @@ export function TrackNewAddress(props: TrackNewAddressProps) {
             }}
         />
         {raw && !loading ?
-            // <Group name="info fetched">
-            <>
-                <MultipleItemsRow>
-                    <MenuEntry>
-                        <Text color="green.400">{raw.data.length}</Text>
-                        <Text fontSize="xs">size bytes</Text>
-                    </MenuEntry>
-                    <MenuEntry >
-                        <Text color="green.400">{Math.round((raw.lamports / LAMPORTS_PER_SOL) * 100) / 100}</Text>
-                        <Text fontSize="xs">sol balance</Text>
-                    </MenuEntry>
-                    <MenuEntry>
-                        <Text color="green.400">{addrFormat(raw.owner, 5)}</Text>
-                        <Text fontSize="xs">owned by program</Text>
-                    </MenuEntry>
-                </MultipleItemsRow>
-                <MenuDivider width={0} />
-                <ActionButton actionVariant="info" action={() => { }}>
-                    <Text>Track</Text>
-                    {raw.data.length == 0 ? <Sublabel >Account has no data to track</Sublabel> : null}
-                </ActionButton>
-            </>
+            (!found ? <ActionButton action={() => { }} >
+                <Text>Account not found</Text>
+                <Sublabel>Either account address is invalid or account not exists</Sublabel>
+            </ActionButton> :
+                // <Group name="info fetched">
+                <>
+                    <MultipleItemsRow>
+                        <MenuEntry>
+                            <Text color="green.400">{raw.data.length}</Text>
+                            <Text fontSize="xs">size bytes</Text>
+                        </MenuEntry>
+                        <MenuEntry >
+                            <Text color="green.400">{Math.round((raw.lamports / LAMPORTS_PER_SOL) * 1000) / 1000}</Text>
+                            <Text fontSize="xs">sol balance</Text>
+                        </MenuEntry>
+                        <MenuEntry>
+                            <Text color="green.400">{addrFormat(raw.owner, 5)}</Text>
+                            <Text fontSize="xs">owned by program</Text>
+                        </MenuEntry>
+                    </MultipleItemsRow>
+                    <MenuDivider width={0} />
+                    <ActionButton actionVariant="info" action={() => { }}>
+                        <Text>Track</Text>
+                        {raw.data.length == 0 ? <Sublabel >Account has no data to track</Sublabel> : null}
+                    </ActionButton>
+                </>)
             // </Group> 
             : <>
                 <Skeleton width={"100%"} height="100px"></Skeleton>
